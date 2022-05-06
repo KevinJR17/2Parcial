659bb6828568812a15d161a63b7acf8cb7da0b67
kevin_huasco <kevin.khza.04@gmail.com>
2022-05-06 10:15:30 -0400

    segundo_componente

---
+++ src/components/segundo_componente.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function segundo_componente() {
+  return (
+    <div class="segundo_componente">
+    <p>Description of what is done here</p>
+    <div class="Cardb-1">
+        <span>Last Updated</span>
+        <span>3/8/2020</span>
+    </div>
+</div>
+  )
+}
+
+export default segundo_componente
